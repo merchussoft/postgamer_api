2708b88ba28bc8b7b8ed58bb366128b7e22e4107
MERCHUS-SERVER\merch <merchussoft@hotmail.com>
2024-03-05 22:49:27 -0500

    se hace un nuevo ajuste para el envio de notificaciones al telegram

--- src/app/helpers/leerArchivoUserIdJson.js
+++ src/app/helpers/leerArchivoUserIdJson.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
+const path = require("path");
 
 exports.leerArchivoJson = (url_archivo_json) => {
+
+    url_archivo_json = path.join(__dirname, '../../..',url_archivo_json);
 
     if (!fs.existsSync(url_archivo_json)) {
         console.log('El archivo de usuarios no existe.');
--- src/app/helpers/telegramSendMessage-helper.js
+++ src/app/helpers/telegramSendMessage-helper.js
@@ -2,59 +2,25 @@ const axios = require('axios');
 const {leerArchivoJson} = require('./leerArchivoUserIdJson');
 const cfg = require("../config/Config");
 
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(cfg.getEnvironment('BOT_TOKEN_TELEGRAM'));
+
 const sendMessageTelegramApiPhoto = (data = {}) => {
-
-    if (leerArchivoJson(cfg.getEnvironment('FILENAME_JSON_USERID')).length) {
-        leerArchivoJson(cfg.getEnvironment('FILENAME_JSON_USERID')).forEach((user, index) => {
-            // Enviar la solicitud POST a la API de Telegram
-            axios.post(`${process.env.URL_API_TELEGRAM}bot${process.env.BOT_TOKEN_TELEGRAM}/sendPhoto`, {
-                chat_id: user,
-                photo: data.url_image,
-                caption: `<strong>${data.title}</strong> <a href='${data.leermas}'>Leer más >></a>`,
-                parse_mode: 'HTML'
-            }).catch((error) => {
-                    console.error('Error al enviar el mensaje y la imagen:', error);
-                });
-        })
-    } else {
-        // Enviar la solicitud POST a la API de Telegram
-        axios.post(`${process.env.URL_API_TELEGRAM}bot${process.env.BOT_TOKEN_TELEGRAM}/sendPhoto`, {
-            chat_id: process.env.CHAT_ID_BOT_TELEGRAM,
-            photo: data.url_image,
-            caption: `<strong>${data.title}</strong> <a href='${data.leermas}'>Leer más >></a>`,
-            parse_mode: 'HTML'
-        }).catch((error) => {
-                console.error('Error al enviar el mensaje y la imagen:', error);
-            });
-    }
-
-
+    bot.sendPhoto(cfg.getEnvironment('CHAT_ID_BOT_TELEGRAM'), data.url_image, {
+        caption: `<strong>${data.title}</strong> <a href='${data.leermas}'>Leer más >>></a>`,
+        parse_mode: 'HTML'
+    }).catch((error) => {
+        console.error('Error al enviar el mensaje:', error);
+    })
 }
 
-
 const sendMessageTelegramApiText = (data = {}) => {
-
-    if (leerArchivoJson(cfg.getEnvironment('FILENAME_JSON_USERID')).length) {
-        leerArchivoJson(cfg.getEnvironment('FILENAME_JSON_USERID')).forEach((user, index) => {
-            // Enviar la solicitud POST a la API de Telegram
-            axios.post(`${process.env.URL_API_TELEGRAM}bot${process.env.BOT_TOKEN_TELEGRAM}/sendMessage`, {
-                chat_id: user,
-                text: `<b>${data.title}</b> \n <a href='${data.leermas}'>Leer más >></a>`,
-                parse_mode: 'HTML'
-            }).catch((error) => {
-                    console.error('Error al enviar el mensaje y la imagen:', error);
-                });
-        })
-    } else {
-        // Enviar la solicitud POST a la API de Telegram
-        axios.post(`${process.env.URL_API_TELEGRAM}bot${process.env.BOT_TOKEN_TELEGRAM}/sendMessage`, {
-            chat_id: process.env.CHAT_ID_BOT_TELEGRAM,
-            text: `<b>${data.title}</b> \n <a href='${data.leermas}'>Leer más >></a>`,
-            parse_mode: 'HTML'
-        }).catch((error) => {
-                console.error('Error al enviar el mensaje y la imagen:', error);
-            });
-    }
+    bot.sendMessage(cfg.getEnvironment('CHAT_ID_BOT_TELEGRAM'), `<b>${data.title}</b> \n <a href='${data.leermas}'>Leer más >></a>`, {
+        parse_mode: 'HTML'
+    }).catch((error) => {
+        console.error('Error al enviar el mensaje:', error);
+    });
 }
 
 
