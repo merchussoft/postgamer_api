cdc4abeb5b05c51a75873b9a42e8ef2ba7d26904
MERCHUS-SERVER\merch <merchussoft@hotmail.com>
2024-04-01 15:42:49 -0500

    se ajusta la carga y muestrra de imagenes ya que no me la muestra como miniatura

--- src/app/routers/post-route.js
+++ src/app/routers/post-route.js
@@ -5,7 +5,7 @@ const {upload} = require('../helpers/multer-upload-file');
 
 route.get('/', listarPost);
 
-route.get('/obtener_imagen/:cod', obtenerImagen);
+route.get('/uploads/:cod/:filename', obtenerImagen);
 route.post('/savenews', upload.single('imagen'), insertNews);
 route.get('/detailspost/:cod_post', detailPost);
 route.post('/upload_to_minio', upload.single('file_minio'), uploadToMinio);
